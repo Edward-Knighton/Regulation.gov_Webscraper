d20deee2d8fcd14618072239fa7855d555a60ed0
edknight <edknight@umich.edu>
2018-07-23 18:34:53 -0400

    basically done i think, need to bugtest on other computers

--- downloadMult.py
+++ downloadMult.py
@@ -1,11 +1,12 @@
 import http.client
 import json
 import urllib.request
+from tqdm import tqdm
 
 def getAll(doc_ID,api_key,page):
     conn = http.client.HTTPConnection("api.data.gov")
 
-    url = 'https://api.data.gov:443/regulations/v3/documents.json?api_key=apiKey&countsOnly=0&encoded=1&dktid=docID&po=PAGE'
+    url = 'https://api.data.gov:443/regulations/v3/documents.json?api_key=apiKey&countsOnly=0&encoded=1&dktid=docID&po=PAGE&rpp=1000'
     url = url.replace("apiKey", api_key)
     url = url.replace("docID", doc_ID)
     url = url.replace("PAGE",page)
@@ -33,53 +34,93 @@ def getOne(doc_ID, api_key):
 
     return parsed_json
 
-def downloadAttachment(dict):
-    url = dict['attachments'][0]['fileFormats'][0]
-    print("Attachment Title: ", dict['attachments'][0]['title'])
-    apiKEY = "&api_key=RcXuvUWp7vXEOnU3JpOh1pwEWGXd4sCmccwQC9gm"
-    url = url + apiKEY
-    fileLocation = 'C:/Users/EJ Knighton/Dropbox/Stuff for EJ/china docket/pdfs/' + dict['attachments'][0]['title'] + '.pdf'
-    urllib.request.urlretrieve(url, fileLocation)
+def downloadAttachment(dict, fh, folderName):
+    count = 0
+    for attachment in dict['attachments']:
+        print()
+        if dict['attachments'][count]['postingRestriction'] == 'No_restrictions':
+            url = dict['attachments'][count]['fileFormats'][0]
+            print("Attachment Title ",count+1,": ", dict['attachments'][count]['title'], file=fh)
+            apiKEY = "&api_key=RcXuvUWp7vXEOnU3JpOh1pwEWGXd4sCmccwQC9gm"
+            url = url + apiKEY
+            fileLocation = folderName +'/' + dict['attachments'][0]['title'] + '.pdf'
+            urllib.request.urlretrieve(url, fileLocation)
+        else:
+            print('Attachment Reason Restricted: ', dict['attachments'][count]['reasonRestricted'], file=fh)
+        count = count + 1
     return
 
-def printLabels(dict,label):
+def printLabels(dict,label, fh):
     str = label + ': ' + dict[label]
-    print(str)
+    print(str, file=fh)
     return
 
-def individual(submit,CONST_API_KEY, count):
-    print("count: ", count)
+def individual(submit,CONST_API_KEY, count, fh, folderName):
+    print("Submit number: ", count, file=fh)
     if 'submitterName' not in submit:
         return
-    printLabels(submit,'submitterName')
-    printLabels(submit,'documentId')
-    printLabels(submit,'commentText')
-
+    printLabels(submit,'submitterName', fh)
+    printLabels(submit,'documentId', fh)
+    if 'organization' in submit:
+        print('Organization: ', submit['organization'], file=fh)
+    printLabels(submit,'commentText', fh)
     if submit['attachmentCount'] != 0:
         singular = getOne(submit['documentId'],CONST_API_KEY)
-        downloadAttachment(singular)
+        downloadAttachment(singular, fh, folderName)
 
-    print()
-    print()
+    print(file=fh)
+    print(file=fh)
     return
 
 
 #only prints 1 page of results currently
 #need to set it up so that it does all of them
+def main():
+    print("This is a tool to download comments from the regulations.gov website")
+    doc_ID = input("What is the Docket ID of the regulation you are trying to download comments from?  :  ")
+    #doc_ID = "USTR-2018-0005"
+    CONST_API_KEY = "RcXuvUWp7vXEOnU3JpOh1pwEWGXd4sCmccwQC9gm"
 
-doc_ID = "USTR-2018-0005"
-CONST_API_KEY = "RcXuvUWp7vXEOnU3JpOh1pwEWGXd4sCmccwQC9gm"
-dict = getAll(doc_ID, CONST_API_KEY, '0')
-totalNumRecords = dict['totalNumRecords']
-totalPages = (totalNumRecords / 25) + 1
-currentPage = 0
-count = 0
-while currentPage < totalPages-1:
+    dict = getAll(doc_ID, CONST_API_KEY, '0')
+    totalNumRecords = dict['totalNumRecords']
+    totalPages = (totalNumRecords / 1000) + 1
+
+    print("Due to Gov API, this program can only download 1000 submits per hour")
+    print("This doc_ID has a total of ",totalNumRecords,"submits")
+    print("What submit would you like to start on?")
+    pageOffsetstr = input("(It would be 0 if starting from the first submit)  :  ")
+    pageOffset = int(pageOffsetstr)
+
+    max = input("How many submits would you like to download?  :  ")
+    print("What is the name of the folder that you would like the attachments saved to?")
+    folderName = input("(Folder must be in the same as the python script)  :  ")
+
+    max = int(max)
+    max = max + pageOffset
+    maxstr = str(max)
+    fileName = "Submits_From_" + pageOffsetstr + "_to_" + maxstr + "_from_DOCID_" + doc_ID + ".txt"
+
+    fh = open(fileName,"w")
+    count = pageOffset
+    pbar = tqdm(total=max-pageOffset)
+
     docs = dict['documents']
     for submit in docs:
-        individual(submit, CONST_API_KEY, count)
-        count = count + 1
-    currentPage = currentPage + 1
-    pageOffset = currentPage * 25
-    pageOffsetstr = str(pageOffset)
+        if count < max:
+            individual(submit, CONST_API_KEY, count, fh, folderName)
+            pbar.update(1)
+            count = count + 1
+        else:
+            break
+    #pageOffset = currentPage * 1000
+    #pageOffsetstr = str(pageOffset)
     dict = getAll(doc_ID, CONST_API_KEY, pageOffsetstr)
+    pbar.close()
+    print("Submits can be found in: ", fileName)
+    print("Exiting now")
+    fh.close()
+    return
+
+
+if __name__ == "__main__":
+    main()
