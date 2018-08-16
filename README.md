9d6b281522c5b3697a122d8e86039fb7a38a348b
edknight <edknight@umich.edu>
2018-08-15 22:32:23 -0400

    Working on csv handling

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# Regulation.gov_Webscraper
+# Regulation.gov Webscraper
 
-Built using pdf to text converter pdfminer.six which can be found at: https://github.com/pdfminer/pdfminer.six
+
+--An application that allows users to easily download and analyze comments on all legislature submitted to regulations.gov
+--Users can download all data as a CSV file, a TXT file for text-classification purposes, or simply download all files relating to the legislature for custom use
+
+
+
+
+Built using modules from pdf to text converter pdfminer.six which can be found at: https://github.com/pdfminer/pdfminer.six
--- steel_docket/steel_Data.py
+++ steel_docket/steel_Data.py
@@ -2,19 +2,20 @@ import json
 import glob,os
 #stuff for pdf to txt file
 import csv
+import codecs
 
 
 print("here")
 import os
 for filename in os.listdir(os.getcwd()):
-    print(filename)
     print("here1")
+    print(filename)
     try:
-        with open(filename, newline=' ') as csvfile:
+        with open(filename, 'rU', encoding='mac_roman') as csvfile:
             print("here2")
-            reader = csv.DictReader(csvfile,)
-            for row in reader:
-                print(row[0], row[1])
+            reader = csv.reader(csvfile)
+            for row in reader(row.replace('/0','')):
+                print(', '.join(row))
     except IOError as exc:
         if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
             raise # Propagate other kinds of IOError.
